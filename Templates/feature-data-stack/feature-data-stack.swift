9d58b324ad0f10b90cc27ba2f024de67791483f2
juyeon <remake867@gmail.com>
2025-09-24 22:15:13 +0900

    init

---
+++ Templates/feature-data-stack/feature-data-stack.swift
@@ -0,0 +1,18 @@
+import ProjectDescription
+
+let name: Template.Attribute = .required("Name")     // e.g. Post
+
+let template = Template(
+  description: "repo - query scaffold",
+  attributes: [name],
+  items: [
+    .file(
+      path: "Features/\(name)/Domain/\(name)Repository.swift",
+      templatePath: "Repository.swift.stencil"
+    ),
+    .file(
+      path: "Features/\(name)/Query/\(name)QueryService.swift",
+      templatePath: "QueryService.swift.stencil"
+    ),
+  ]
+)
---
+++ Templates/feature-usecase/feature-usecase.swift
@@ -0,0 +1,16 @@
+
+import ProjectDescription
+
+let action: Template.Attribute = .required("Action") // e.g. Load, ToggleLike
+let name: Template.Attribute = .required("Name")     // e.g. Post
+
+let template = Template(
+  description: "Single UseCase scaffold",
+  attributes: [action, name],
+  items: [
+    .file(
+      path: "Features/\(name)/Domain/\(action)\(name)UseCase.swift",
+      templatePath: "UseCase.swift.stencil"
+    ),
+  ]
+)
