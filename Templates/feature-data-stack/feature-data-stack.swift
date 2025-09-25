06a624d6a8d41a5dbabe6c1322ab0dae29a7e2e7
juyeon <remake867@gmail.com>
2025-09-25 10:26:27 +0900

    add stencil

--- Templates/feature-data-stack/feature-data-stack.swift
+++ Templates/feature-data-stack/feature-data-stack.swift
@@ -1,18 +1,23 @@
 import ProjectDescription
 
-let name: Template.Attribute = .required("Name")     // e.g. Post
+let name: Template.Attribute = .required("Name")
 
 let template = Template(
   description: "requred: name",
   attributes: [name],
   items: [
     .file(
-      path: "Features/\(name)/Domain/\(name)Repository.swift",
+      path: "Features/\(name)/Data/\(name)Repository.swift",
       templatePath: "Repository.swift.stencil"
     ),
     .file(
-      path: "Features/\(name)/Query/\(name)QueryService.swift",
+      path: "Features/\(name)/Data/\(name)QueryService.swift",
       templatePath: "QueryService.swift.stencil"
+    ),
+    
+    .file(
+      path: "Features/\(name)/Usecase/\(name)Usecase.swift",
+      templatePath: "Usecase.swift.stencil"
     ),
   ]
 )
